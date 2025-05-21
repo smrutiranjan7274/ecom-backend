f9076bae39147cc4256d5946ac73be2d54fbb933
Smruti Ranjan Bhanja <smrutibhanja32@gmail.com>
2025-05-21 06:20:43 +0530

    Remove unused configuration metadata and sample data SQL file

--- src/main/resources/data.sql
+++
@@ -1,9 +0,0 @@
-INSERT INTO products
-    (name, description, price, image_url, stock_quantity, category)
-VALUES
-    ('Laptop', 'High performance laptop with 16GB RAM', 999.99, 'https://bankofelectronics.com/905-medium_default/macbook.jpg', 10, 'Electronics'),
-    ('Smartphone', 'Latest smartphone with 128GB storage', 699.99, 'https://www.apple.com/newsroom/images/2024/09/apple-debuts-iphone-16-pro-and-iphone-16-pro-max/tile/Apple-iPhone-16-Pro-hero-240909-lp.jpg.landing-big_2x.jpg', 15, 'Electronics'),
-    ('Headphones', 'Wireless noise-cancelling headphones', 199.99, 'https://m.media-amazon.com/images/I/41kW26ceLtL.jpg', 20, 'Electronics'),
-    ('T-Shirt', 'Cotton t-shirt with premium finish', 29.99, 'https://www.cubelelo.com/cdn/shop/products/CubeleloLogoWhite_400x.jpg?v=1647428451', 50, 'Clothing'),
-    ('Coffee Mug', 'Ceramic mug with heat retention', 12.99, 'https://templecoffee.com/cdn/shop/files/PicoCollection_Mug_400x.jpg?v=1713194309', 30, 'Home'),
-    ('Jeans', 'Rugged jeans with premium finish	', 39.99, 'https://s.alicdn.com/@sc04/kf/Hb86a2aa0d3b249cfac3f19e12015e5ddg.png_720x720q50.png', 60, 'Clothing');
